ad22d7bddb3bdd58b04465d08e9226bab34c4df3
Knight~ <x665adex563@gmail.com>
2026-04-11 11:50:09 +0800

    add locale routing, controller support, and navbar language  switcher

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Jdstore20260410
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :"zh-TW"]
 
     # Configuration for the application, engines, and railties goes here.
     #
