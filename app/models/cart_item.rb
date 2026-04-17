74fb572c9a86ed3ce463ab1ad838960d4fa15cc6
Knight~ <x665adex563@gmail.com>
2026-04-17 15:43:19 +0800

    feat(cart): allow users to update quantity of items in cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,17 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :quantity, presence: { message: I18n.t("cart_items.errors.blank") }
+  validates :quantity, numericality: { greater_than: 0, message: I18n.t("cart_items.errors.greater_than_zero") }
+
+  validate :check_stock
+
+  def check_stock
+    return unless quantity && product
+
+    if quantity > product.quantity
+      errors.add(:quantity, t("cart_items.errors.exceed_stock"))
+    end
+  end
 end
