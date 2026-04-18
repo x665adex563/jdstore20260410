1509ff72649cdb20393e5fbaa1b43c43ffd0e149
Knight~ <x665adex563@gmail.com>
2026-04-18 10:21:01 +0800

    feat(cart-checkout): add checkout action and orders routes andcheckout page with cart cummary and order form

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
         get :checkout
       end
     end
+    resources :orders
 
     # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
     # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
