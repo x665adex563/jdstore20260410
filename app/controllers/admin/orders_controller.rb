d1621410abca6a87b66f065c51e08e35dcacf6f2
Knight~ <x665adex563@gmail.com>
2026-04-20 15:12:53 +0800

    feat(admin): add admin order state change actions and correspending buttons

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,4 +12,28 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find_by!(token: params[:id])
     @product_lists = @order.product_lists
   end
+
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_back(fallback_location: admin_orders_path)
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_back(fallback_location: admin_orders_path)
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
+    redirect_back(fallback_location: admin_orders_path)
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_back(fallback_location: admin_orders_path)
+  end
 end
