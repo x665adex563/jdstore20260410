9161ff1e8b52e2e1cb4a149113ceb37510650d8b
Knight~ <x665adex563@gmail.com>
2026-04-20 14:12:31 +0800

    feat(admin): add admin order show page and helper to display paid state

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,4 +7,9 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.order(id: :desc)
   end
+
+  def show
+    @order = Order.find_by!(token: params[:id])
+    @product_lists = @order.product_lists
+  end
 end
