e760031e050c318bac46ff31d1e2c51ab033c4ab
Knight~ <x665adex563@gmail.com>
2026-04-29 10:47:58 +0800

    feat(storage): use S3 as Actice Storage service in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
+  
+  config.active_storage.service = :amazon
+
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
